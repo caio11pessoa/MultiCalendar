5df47c697194a533c0123201affe85bf972bfacf
CaioPessoa <caiopessoa1298@gmail.com>
2025-08-25 20:03:22 -0300

    add project

---
+++ MultiCalendar/MultiCalendarApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiCalendarApp.swift
+//  MultiCalendar
+//
+//  Created by Caio de Almeida Pessoa on 20/08/25.
+//
+
+import SwiftUI
+
+@main
+struct MultiCalendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
